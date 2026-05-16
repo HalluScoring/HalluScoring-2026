2be9551110f2b5f9d5ab2d09e1f5c8c645e886b1
Ahmed Hasanaath <61200955+gufranSabri@users.noreply.github.com>
2026-05-16 10:37:20 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/HalluScoring-2026',
+  base: '/HalluScoring-2026/',
 })
